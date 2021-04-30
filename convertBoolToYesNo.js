9afb4a53c6eca1a481ffe2ec3dab20e3551caa0b
Scott Gorsky <sgorsky@gmail.com>
2021-04-29 21:53:06 -0500

    convert boolean to yes or no

--- convertBoolToYesNo.js
+++ convertBoolToYesNo.js
@@ -1,4 +1,4 @@
-//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. This is a test to connect git/github.
 
 
 function boolToWord( bool ){
