8e93f3e37b55f82a2de7cf76929b186bc2ed1d36
Scott Gorsky <sgorsky@gmail.com>
2021-04-30 21:04:17 -0500

    Add files via upload

---
+++ repeatedString.js
@@ -0,0 +1,13 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  let repeatedString = ''
+  while (n > 0){
+    repeatedString += s;
+    n--;
+  }
+  return repeatedString;
+}
