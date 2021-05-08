19944c030db6acbdabf5a5148fe446cce447e815
Scott Gorsky <sgorsky@gmail.com>
2021-05-08 09:05:44 -0500

    Add files via upload

---
+++ sumTheStrings.js
@@ -0,0 +1,7 @@
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+
+function sumStr(a,b) {
+  let sum = Number(+a + +b)
+  return sum.toString()
+}
