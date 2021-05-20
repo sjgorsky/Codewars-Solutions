0dd17645ec896c62ec4db3772656d0bd60af1370
Scott Gorsky <sgorsky@gmail.com>
2021-05-20 15:58:04 -0500

    Add files via upload

---
+++ tailDebug.js
@@ -0,0 +1,13 @@
+//debugging exercise
+
+
+function correctTail(body, tail){
+  
+  let sub = body.substr(body.length-tail.length)
+  
+  if (sub === tail){
+    return true
+  }
+  else 
+    return false
+  }
