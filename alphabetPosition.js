e33209a7449b1263042ec1cfbf47f417217599be
Scott Gorsky <sgorsky@gmail.com>
2021-06-21 21:32:19 -0500

    Add files via upload

---
+++ alphabetPosition.js
@@ -0,0 +1,10 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
+function position(letter){
+  const position = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  return `Position of alphabet: ${position.indexOf(letter) + 1}`
