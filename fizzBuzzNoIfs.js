9c825a99f23396e16f8806403c79cb007ba4340e
Scott Gorsky <sgorsky@gmail.com>
2021-06-23 19:52:26 -0500

    Add files via upload

---
+++ fizzBuzzNoIfs.js
@@ -0,0 +1,8 @@
+//Do 'FizzBuzz without 'ifs'
+
+const fizzBuzz = n => {
+  return (n % 15 === 0 && "FizzBuzz") || 
+         (n % 3 === 0 && "Fizz") || 
+         (n % 5 == 0 && "Buzz") || 
+         n
+}
