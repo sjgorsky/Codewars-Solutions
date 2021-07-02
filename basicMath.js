df07649d87a979879dab827b3708415d282d1465
Scott Gorsky <sgorsky@gmail.com>
2021-07-01 20:19:19 -0500

    Add files via upload

---
+++ basicMath.js
@@ -0,0 +1,16 @@
+//return basic math operation based on value of operation parameter
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
