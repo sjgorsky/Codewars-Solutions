96537cba9f4507af104219c389d63a4257564439
Scott Gorsky <sgorsky@gmail.com>
2021-07-12 21:02:18 -0500

    Add files via upload

---
+++ fizzBuzzArray.js
@@ -0,0 +1,18 @@
+//return FizzBuzz in an array
+
+
+function fizzbuzz(n){
+  const arr = []
+  for (let i = 1; i <= n; i++){
+    if(i % 15 ==0){
+      arr.push('FizzBuzz')
+    } else if(i % 3 == 0) {
+      arr.push('Fizz')
+    } else if (i % 5 == 0){
+      arr.push('Buzz')
+    } else {
+      arr.push(i)
+      }
+    }
+  return arr
+  }
