99308a9ba8beeedf1a7d54488851fcc6d8a712bd
Scott Gorsky <sgorsky@gmail.com>
2021-07-28 21:34:57 -0500

    Add files via upload

---
+++ multiplyEightOrNine.js
@@ -0,0 +1,9 @@
+//multiply even number by 8 or odd number by 9
+
+function simpleMultiplication(number) {
+    if (number % 2 == 0){
+      return number * 8
+    } else {
+      return number * 9
+    }
+}
