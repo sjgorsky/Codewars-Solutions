e54ad9907586ee591dce78798f2b55f7c07c18a9
Scott Gorsky <sgorsky@gmail.com>
2021-08-07 21:01:03 -0500

    Add files via upload

---
+++ UFCswitch.js
@@ -0,0 +1,6 @@
+//could use switch case here too
+
+
+var quote = function(fighter) {
+  return fighter.toLowerCase() === "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
