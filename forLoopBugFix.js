9b5e8f778db388cf4e2429a12a85bb9d127edb82
Scott Gorsky <sgorsky@gmail.com>
2021-08-26 14:49:18 -0500

    Add files via upload

---
+++ forLoopBugFix.js
@@ -0,0 +1,12 @@
+//fix a for loop
+
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
