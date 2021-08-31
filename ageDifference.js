37242e03184a2e6377a7abb6c9f6d7ed5e664a93
Scott Gorsky <sgorsky@gmail.com>
2021-08-31 15:09:50 -0500

    Add files via upload

---
+++ ageDifference.js
@@ -0,0 +1,11 @@
+//return an array with the difference in ages
+
+
+function differenceInAges (ages) {
+
+    let max = Math.max(...ages),
+        min = Math.min(...ages)
+        diff = max - min
+        
+    return [min, max, diff]
+}
