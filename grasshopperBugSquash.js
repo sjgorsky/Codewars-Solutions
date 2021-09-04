aa91bfce34fc321fbafc23a5e709d67c23a58f04
Scott Gorsky <sgorsky@gmail.com>
2021-09-03 21:30:58 -0500

    Add files via upload

---
+++ grasshopperBugSquash.js
@@ -0,0 +1,14 @@
+//create functions in order
+
+var health = 100
+var position = 0
+var coins = 0
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
