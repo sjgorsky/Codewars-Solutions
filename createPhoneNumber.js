51dca5e0f9e4ac8225aaa1022ff8eab7f2a80e57
Scott Gorsky <sgorsky@gmail.com>
2021-09-07 20:01:15 -0500

    Add files via upload

---
+++ createPhoneNumber.js
@@ -0,0 +1,9 @@
+//create formatted phone number from array
+
+
+function createPhoneNumber(numbers){
+  let areaCode = numbers.slice(0,3).join('')
+  let firstThree = numbers.slice(3, 6).join('')
+  let lastFour = numbers.slice(6).join('')
+  return `(${areaCode}) ${firstThree}-${lastFour}`
+}
