145b03e2257e28e7bdb16403e91263592ebdc47e
Scott Gorsky <sgorsky@gmail.com>
2021-09-11 21:13:53 -0500

    Add files via upload

---
+++ validParentheses.js
@@ -0,0 +1,13 @@
+//check if parentheses are valid
+
+
+function validParentheses(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+  
+  return n == 0;
+}
