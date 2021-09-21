79bdd5eb730fb5744b9b4e8a45c3851715277370
Scott Gorsky <sgorsky@gmail.com>
2021-09-20 20:16:54 -0500

    Add files via upload

---
+++ creditCardMask.js
@@ -0,0 +1,17 @@
+// return masked string
+function maskify(cc) {
+    if (cc.length > 4) {
+    let result = '';
+    return cc.split('').reduce((acc, cur, i) => {
+      if (cc.length - i > 4) {
+        result += '#';
+      } else {
+        result += cur;
+      }
+      return result;
+    }, '');
+  } else {
+    return cc;
+  }
+
+}
