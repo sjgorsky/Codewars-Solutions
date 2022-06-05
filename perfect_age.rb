e6aa74f3ad9ba08fdf7284a9c95e1702c4a3d37f
Scott Gorsky <sgorsky@gmail.com>
2022-06-05 16:57:27 -0500

    Add files via upload

---
+++ perfect_age.rb
@@ -0,0 +1,5 @@
+def predict_age(a1, a2, a3, a4, a5, a6, a7, a8)
+  sq = [a1 ** 2, a2 ** 2, a3 ** 2, a4 ** 2, a5 ** 2, a6 ** 2, a7 ** 2, a8 ** 2]
+  sum = sq.reduce(:+)
+  (Math.sqrt(sum) / 2).floor
+end
